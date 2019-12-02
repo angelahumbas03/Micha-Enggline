82391950dec855b947a77629fe1a84759b0b1029
Angela Humbas <s21610493@student.unklab.ac.id>
2019-12-02 18:21:53 +0800

    kita ada tambah app.js

--- App.js
+++ App.js
@@ -1,18 +1,61 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AboutScreen from './screens/AboutScreen';
+import AcehScreen from './screens/AcehScreen';
+import BaliScreen from './screens/BaliScreen';
+import BantenScreen from './screens/BantenScreen';
+import BBScreen from './screens/BBScreen';
+import DKIJakartaScreen from './screens/DKIJakartaScreen';
+import GorontaloScreen from './screens/GorontaloScreen';
 import HomeScreen from './screens/HomeScreen';
+import JambiScreen from './screens/JambiScreen';
+import JatengScreen from './screens/JatengScreen';
+import JatimScreen from './screens/JatimScreen';
+import JogjaScreen from './screens/JogjaScreen';
+import KalbarScreen from './screens/KalbarScreen';
+import LampungScreen from './screens/LampungScreen';
+import MalukuScreen from './screens/MalukuScreen';
+import NTBScreen from './screens/NTBScreen';
 import PakaianScreen from './screens/PakaianScreen';
-import DescScreen from './screens/DescScreen';
+import PapuaScreen from './screens/PapuaScreen';
+import RiauScreen from './screens/RiauScreen';
+import SulselScreen from './screens/SulselScreen';
+import SultraScreen from './screens/SultraScreen';
+import SulutScreen from './screens/SulutScreen';
+import SumutScreen from './screens/SumutScreen';
+import VideoScreen from './screens/VideoScreen';
+
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 
 const navigator = createStackNavigator({
   About: AboutScreen,
   Pakaian: PakaianScreen,
+  Video: VideoScreen,
   Home: HomeScreen,
-  Desc: DescScreen,
-},{
+  Sulut: SulutScreen,
+  Sulteng: SultengScreen,
+  Sultra: SultraScreen,
+  Sulsel: SulselScreen,
+  Sumut: SumutScreen,
+  DKIJakarta: DKIJakartaScreen,
+  Gorontalo: GorontaloScreen,
+  Jateng: JatengScreen,
+  Jatim: JatimScreen,
+  Jambi: JambiScreen,
+  Jogja: JogjaScreen,
+  Lampung: LampungScreen,
+  Maluku: MalukuScreen,
+  NTB: NTBScreen,
+  Aceh: AcehScreen,
+  Papua: PapuaScreen,
+  Bali: BaliScreen,
+  BB: BBScreen,
+  Riau: RiauScreen,
+  Banten: BantenScreen,
+  Kalbar: KalbarScreen,
+  
+
   initialRouteName: 'Home',
   defaultNavigationOptions: {
     headerTitle: 'Pakaian Adat'
@@ -24,7 +67,7 @@ export default createAppContainer(navigator);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f12f',
     alignItems: 'center',
     justifyContent: 'center',
   },
